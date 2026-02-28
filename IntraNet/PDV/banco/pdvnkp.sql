5df4addeb52ce3400826fb611a4e8b446a8af026
Ana <anaclarinhakassao@gmail.com>
2026-02-28 11:33:24 -0300

    alterações

---
+++ IntraNet/PDV/banco/pdvnkp.sql
@@ -0,0 +1,45 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.32-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.10.0.7000
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para pdv_cantina
+DROP DATABASE IF EXISTS `pdv_cantina`;
+CREATE DATABASE IF NOT EXISTS `pdv_cantina` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+USE `pdv_cantina`;
+
+-- Copiando estrutura para tabela pdv_cantina.produtos
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `id_produto` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo_barras` varchar(50) NOT NULL DEFAULT '',
+  `nome` varchar(255) NOT NULL DEFAULT '',
+  `preco` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `foto_url` varchar(255) NOT NULL DEFAULT '',
+  `qtd` int(11) NOT NULL,
+  `aapm` tinyint(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2147483648 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Copiando dados para a tabela pdv_cantina.produtos: ~1 rows (aproximadamente)
+DELETE FROM `produtos`;
+INSERT INTO `produtos` (`id_produto`, `codigo_barras`, `nome`, `preco`, `foto_url`, `qtd`, `aapm`) VALUES
+	(1, '123', 'Paçoca Premium DiMarco', 3.30, 'adsdda', 1, 1);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
