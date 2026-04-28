0c243a8b2484bf014143fdc24784aa44d0823e08
Ana Clara <anaclarinhakassao@gmail.com>
2026-04-28 15:52:18 -0300

    reposicao ajustado

--- IntraNet/banco/cantinabkp.sql
+++ IntraNet/banco/cantinabkp.sql
@@ -2,7 +2,7 @@
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           10.4.32-MariaDB - mariadb.org binary distribution
 -- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.15.0.7171
+-- HeidiSQL Versão:              12.10.0.7000
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `cadastro` (
 -- Copiando dados para a tabela cantina.cadastro: ~1 rows (aproximadamente)
 DELETE FROM `cadastro`;
 INSERT INTO `cadastro` (`id_cadastro`, `nome`, `email`, `senha`, `img`) VALUES
-	(1, 'Ana Banana', 'anabanana@gmail.com', 'c64a0fbaee020a68288cd158bbd2d711ef7b8c07b592903546e7bc38fc002f02', 'images/jimin.jpg');
+	(1, 'Ana Banana', 'anabanana@gmail.com', 'b2e7e03ede85560977685add00fa3276ea7aa1c780fc87b870d7b027dc277007', '/imagens/def_avt.jpg');
 
 -- Copiando estrutura para tabela cantina.cardapio_dia
 DROP TABLE IF EXISTS `cardapio_dia`;
@@ -128,13 +128,16 @@ CREATE TABLE IF NOT EXISTS `pedidos` (
   `form_pag` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_pedido`),
   KEY `Index 2` (`id_user`),
-  CONSTRAINT `FK_pedidos_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  CONSTRAINT `FK_pedidos_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Copiando dados para a tabela cantina.pedidos: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela cantina.pedidos: ~4 rows (aproximadamente)
 DELETE FROM `pedidos`;
 INSERT INTO `pedidos` (`id_pedido`, `id_user`, `num_pedido`, `data`, `status`, `valor_total`, `qtd_total`, `form_pag`) VALUES
-	(1, 1, 1, '2026-04-16 20:41:18', '', 46.30, 4, 'DINHEIRO (F2)');
+	(7, 1, 1, '2026-04-16 17:08:06', '', 0.00, 1, 'DINHEIRO (F2)'),
+	(8, 1, 2, '2026-04-16 17:08:19', '', 0.00, 2, 'CARTÃO DE CRÉDITO (F3)'),
+	(9, 1, 3, '2026-04-16 17:09:23', '', 0.00, 2, 'DINHEIRO (F2)'),
+	(10, 1, 4, '2026-04-16 17:10:44', '', 21.76, 2, 'DINHEIRO (F2)');
 
 -- Copiando estrutura para tabela cantina.pedidos_itens
 DROP TABLE IF EXISTS `pedidos_itens`;
@@ -147,27 +150,30 @@ CREATE TABLE IF NOT EXISTS `pedidos_itens` (
   PRIMARY KEY (`id_itens`),
   KEY `Index 2` (`id_pedido`),
   KEY `Index 3` (`id_produto`),
-  CONSTRAINT `FK_pedidos_itens_pedidos` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_pedidos_itens_pedidos` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_pedidos_itens_produtos` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id_produto`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Copiando dados para a tabela cantina.pedidos_itens: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela cantina.pedidos_itens: ~6 rows (aproximadamente)
 DELETE FROM `pedidos_itens`;
 INSERT INTO `pedidos_itens` (`id_itens`, `id_pedido`, `id_produto`, `qtd`, `preco_unitario`) VALUES
-	(1, 1, 3, 1, 3.800000),
-	(2, 1, 2, 1, 3.500000),
-	(3, 1, 6, 2, 19.500000);
+	(12, 7, 1, 1, 2.260000),
+	(13, 8, 14, 1, 18.600000),
+	(14, 8, 1, 1, 2.260000),
+	(15, 9, 14, 1, 18.600000),
+	(16, 9, 1, 1, 2.260000),
+	(17, 10, 6, 1, 19.500000),
+	(18, 10, 1, 1, 2.260000);
 
 -- Copiando estrutura para tabela cantina.produtos
 DROP TABLE IF EXISTS `produtos`;
 CREATE TABLE IF NOT EXISTS `produtos` (
   `id_produto` int(11) NOT NULL AUTO_INCREMENT,
-  `id_categoria` int(11) DEFAULT NULL,
+  `id_categoria` int(11) NOT NULL,
   `codigo_barras` varchar(255) NOT NULL DEFAULT '',
   `nome` varchar(255) NOT NULL DEFAULT '',
   `descricao` text NOT NULL,
   `preco` decimal(10,2) NOT NULL DEFAULT 0.00,
-  `dt_validade` date DEFAULT NULL,
   `qtd` int(11) NOT NULL,
   `img` varchar(255) NOT NULL DEFAULT '',
   `disponivel` int(11) DEFAULT NULL,
@@ -178,75 +184,75 @@ CREATE TABLE IF NOT EXISTS `produtos` (
 
 -- Copiando dados para a tabela cantina.produtos: ~68 rows (aproximadamente)
 DELETE FROM `produtos`;
-INSERT INTO `produtos` (`id_produto`, `id_categoria`, `codigo_barras`, `nome`, `descricao`, `preco`, `dt_validade`, `qtd`, `img`, `disponivel`) VALUES
-	(1, 1, '11', 'Café coado 50ml', '', 2.26, '0000-00-00', 3, '/images/cafe_50.jpg', 1),
-	(2, 1, '3', 'Café coado 100ml', '', 3.50, '0000-00-00', 0, '/images/cafe_100.png', 1),
-	(3, 1, '4', 'Pingado 150ml', '', 3.80, '0000-00-00', 0, '/images/cafe_pingado.png', 1),
-	(4, 1, '', 'Chocolate quente 200ml', '', 6.94, '0000-00-00', 0, '/images/cafe.png', 1),
-	(5, 2, '', 'Arroz, Strogonoff de frango P', '', 18.60, '0000-00-00', 0, '/images/arroz_strog.png', 1),
-	(6, 2, '2', 'Arroz, Strogonoff de frango M', '', 19.50, '0000-00-00', 0, '/images/arroz_strog.jpg', 1),
-	(7, 2, '', 'Arroz, Strogonoff de frango G', '', 20.70, '0000-00-00', 0, '/images/arroz_strog.png', 1),
-	(8, 2, '', 'Arroz, lasanha bolonhesa P', '', 18.60, '0000-00-00', 0, '/images/arroz_lasan.png', 1),
-	(9, 2, '', 'Arroz, lasanha bolonhesa M', '', 19.50, '0000-00-00', 0, '/images/arroz_lasan.png', 1),
-	(10, 2, '', 'Arroz, lasanha bolonhesa G', '', 20.70, '0000-00-00', 0, '/images/arroz_lasan.png', 1),
-	(11, 2, '', 'Arroz, feijão, carne de panela P', '', 18.60, '0000-00-00', 0, '/images/arroz_carp.jpg', 1),
-	(12, 2, '', 'Arroz, feijão, carne de panela M', '', 19.50, '0000-00-00', 0, '/images/arroz_carp.jpg', 1),
-	(13, 2, '', 'Arroz, feijão, carne de panela G', '', 20.70, '0000-00-00', 0, '/images/arroz_carp.jpg', 1),
-	(14, 2, '7', 'Macarrão bolonhesa P', '', 18.60, '0000-00-00', 0, '/images/macarr_bolon.jpg', 1),
-	(15, 2, '', 'Macarrão bolonhesa M', '', 19.50, '0000-00-00', 0, '/images/macarr_bolon.jpg', 1),
-	(16, 2, '', 'Macarrão bolonhesa G', '', 20.70, '0000-00-00', 0, '/images/macarr_bolon.jpg', 1),
-	(17, 3, '', 'Picolé de água', '', 3.06, '0000-00-00', 0, '/images/picole_agua.jpg', 1),
-	(18, 3, '', 'Picolé de leite', '', 4.20, '0000-00-00', 0, '/images/picole_leite.png', 1),
-	(19, 3, '', 'Picolé tipo skimo', '', 5.50, '0000-00-00', 0, '/images/picole_skimo.jpg', 1),
-	(20, 3, '', 'Picolé gianduia', '', 5.50, '0000-00-00', 0, '/images/img_ntf.png', 1),
-	(21, 3, '', 'Picolé Maxxi leite trufado', '', 9.00, '0000-00-00', 0, '/images/maxxi_black.png', 1),
-	(22, 3, '', 'Picolé Maxxi Black', '', 9.00, '0000-00-00', 0, '/images/maxxi_black.png', 1),
-	(23, 3, '', 'Picolé Maxxi White', '', 9.00, '0000-00-00', 0, '/images/maxxi_white.png', 1),
-	(24, 3, '', 'Copo Big bombom', '', 7.00, '0000-00-00', 0, '/images/big_bombom.png', 1),
-	(25, 3, '', 'Copo Big flocos', '', 7.00, '0000-00-00', 0, '/images/big_flocos.png', 1),
-	(26, 3, '', 'Copo Big napolitano', '', 7.00, '0000-00-00', 0, '/images/big_napolitano.png', 1),
-	(27, 3, '', 'Copo Big speciale', '', 7.00, '0000-00-00', 0, '/images/img_ntf.png', 1),
-	(28, 3, '', 'Mini bombom Maxxi açaí', '', 15.50, '0000-00-00', 0, '/images/bomb_acai.jpg', 1),
-	(29, 3, '', 'Mini bombom Maxxi skimo', '', 15.50, '0000-00-00', 0, '/images/bomb_skimo.png', 1),
-	(30, 3, '', 'Pote de açaí 240ml', '', 12.50, '0000-00-00', 0, '/images/acai_240.png', 1),
-	(31, 3, '', 'Sorvete misto', '', 2.00, '0000-00-00', 0, '/images/picole_misto.png', 1),
-	(32, 4, '', 'Esfirra de carne', '', 7.29, '0000-00-00', 0, '/images/esfirra.png', 1),
-	(33, 4, '', 'Esfirra de frango catu', '', 7.29, '0000-00-00', 0, '/images/esfirra.png', 1),
-	(34, 4, '', 'Enrolado de queijo', '', 7.29, '0000-00-00', 0, '/images/enroladinho.jpg', 1),
-	(35, 4, '', 'Enrolado de salsicha', '', 7.29, '0000-00-00', 0, '/images/enr_salsi.png', 1),
-	(36, 4, '', 'Hambúrguer com cheddar', '', 7.29, '0000-00-00', 0, '/images/bauru.png', 1),
-	(37, 4, '', 'Assado calabresa com queijo', '', 7.29, '0000-00-00', 0, '/images/torta.png', 1),
-	(38, 4, '', 'Torta frango catu tomate', '', 8.14, '0000-00-00', 0, '/images/torta.png', 1),
-	(39, 4, '', 'Torta presunto queijo catu', '', 8.14, '0000-00-00', 0, '/images/torta.png', 1),
-	(40, 4, '', 'Coxinha de carne', '', 7.58, '0000-00-00', 0, '/images/coxinha.jpg', 1),
-	(41, 4, '4', 'Coxinha de frango', '', 7.58, '0000-00-00', 0, '/images/coxinha.jpg', 1),
-	(42, 4, '', 'Coxinha de costela', '', 8.50, '0000-00-00', 0, '/images/coxinha.jpg', 1),
-	(43, 5, '', 'Pão com ovo', '', 5.62, '0000-00-00', 0, '/images/pao_ovo.jpg', 1),
-	(44, 5, '', 'Bauru', '', 10.66, '0000-00-00', 0, '/images/bauru.png', 1),
-	(45, 5, '', 'Americano', '', 18.42, '0000-00-00', 0, '/images/img_ntf.png', 1),
-	(46, 5, '', 'Omelete simples', '', 5.00, '0000-00-00', 0, '/images/omelete_sim.png', 1),
-	(47, 5, '', 'Omelete presunto e queijo', '', 7.50, '0000-00-00', 0, '/images/omelete_sim.png', 1),
-	(48, 5, '', 'Ovo mexido', '', 4.50, '0000-00-00', 0, '/images/ovo_mexido.png', 1),
-	(49, 5, '', 'Misto quente', '', 6.50, '0000-00-00', 0, '/images/misto_quente.png', 1),
-	(50, 6, '', 'Trento avelã', '', 4.11, '0000-00-00', 0, '/images/trento_avela.jpg', 1),
-	(51, 6, '', 'Trento chocolate', '', 4.11, '0000-00-00', 0, '/images/trento_choc.jpg', 1),
-	(52, 6, '', 'Stikadinho', '', 2.00, '0000-00-00', 0, '/images/stikadinho.jpg', 1),
-	(53, 6, '', 'Halls morango', '', 2.50, '0000-00-00', 0, '/images/halls_mor.png', 1),
-	(54, 6, '', 'Paçoca', '', 3.00, '0000-00-00', 0, '/images/pacoca.jpg', 1),
-	(55, 7, '', 'Trufa de brigadeiro', '', 6.00, '0000-00-00', 0, '/images/trufa.png', 1),
-	(56, 7, '', 'Trufa de beijinho', '', 6.00, '0000-00-00', 0, '/images/trufa.png', 1),
-	(57, 7, '', 'Trufa de ninho', '', 6.00, '0000-00-00', 0, '/images/trufa.png', 1),
-	(58, 7, '', 'Trufa Ovomaltine', '', 6.50, '0000-00-00', 0, '/images/trufa.png', 1),
-	(59, 7, '', 'Trufa Nutella', '', 6.50, '0000-00-00', 0, '/images/trufa.png', 1),
-	(60, 7, '', 'Trufa Maracujá', '', 6.50, '0000-00-00', 0, '/images/trufa.png', 1),
-	(61, 7, '', 'Trufa Oreo', '', 6.50, '0000-00-00', 0, '/images/trufa.png', 1),
-	(62, 7, '', 'Bala baiana', '', 6.00, '0000-00-00', 0, '/images/bala_baiana.png', 1),
-	(63, 8, '', 'Mini Coca-Cola', '', 3.00, '0000-00-00', 0, '/images/coca_200.png', 1),
-	(64, 8, '', 'Mini Fanta', '', 3.00, '0000-00-00', 0, '/images/fanta_200.png', 1),
-	(65, 8, '', 'Água', '', 2.69, '0000-00-00', 0, '/images/agua.jpg', 1),
-	(66, 8, '', 'Água com gás', '', 2.70, '0000-00-00', 0, '/images/agua_gas.jpg', 1),
-	(67, 8, '', 'Coca-Cola 2L', '', 11.97, '0000-00-00', 0, '/images/coca_2l.png', 1),
-	(68, 8, '', 'Fanta 2L', '', 11.50, '0000-00-00', 0, '/images/fanta_2l.png', 1);
+INSERT INTO `produtos` (`id_produto`, `id_categoria`, `codigo_barras`, `nome`, `descricao`, `preco`, `qtd`, `img`, `disponivel`) VALUES
+	(1, 1, '11', 'Café coado 50ml', '', 2.26, 3, 'cafe_50.jpg', 1),
+	(2, 1, '3', 'Café coado 100ml', '', 3.50, 0, 'cafe_100.png', 1),
+	(3, 1, '4', 'Pingado 150ml', '', 3.80, 0, 'cafe_pingado.png', 1),
+	(4, 1, '', 'Chocolate quente 200ml', '', 6.94, 0, 'cafe.png', 1),
+	(5, 2, '', 'Arroz, Strogonoff de frango P', '', 18.60, 0, 'arroz_strog.png', 1),
+	(6, 2, '2', 'Arroz, Strogonoff de frango M', '', 19.50, 0, 'arroz_strog.jpg', 1),
+	(7, 2, '', 'Arroz, Strogonoff de frango G', '', 20.70, 0, 'arroz_strog.png', 1),
+	(8, 2, '', 'Arroz, lasanha bolonhesa P', '', 18.60, 0, 'arroz_lasan.png', 1),
+	(9, 2, '', 'Arroz, lasanha bolonhesa M', '', 19.50, 0, 'arroz_lasan.png', 1),
+	(10, 2, '', 'Arroz, lasanha bolonhesa G', '', 20.70, 0, 'arroz_lasan.png', 1),
+	(11, 2, '', 'Arroz, feijão, carne de panela P', '', 18.60, 0, 'arroz_carp.jpg', 1),
+	(12, 2, '', 'Arroz, feijão, carne de panela M', '', 19.50, 0, 'arroz_carp.jpg', 1),
+	(13, 2, '', 'Arroz, feijão, carne de panela G', '', 20.70, 0, 'arroz_carp.jpg', 1),
+	(14, 2, '7', 'Macarrão bolonhesa P', '', 18.60, 0, 'macarr_bolon.jpg', 1),
+	(15, 2, '', 'Macarrão bolonhesa M', '', 19.50, 0, 'macarr_bolon.jpg', 1),
+	(16, 2, '', 'Macarrão bolonhesa G', '', 20.70, 0, 'macarr_bolon.jpg', 1),
+	(17, 3, '', 'Picolé de água', '', 3.06, 0, 'picole_agua.jpg', 1),
+	(18, 3, '', 'Picolé de leite', '', 4.20, 0, 'picole_leite.png', 1),
+	(19, 3, '', 'Picolé tipo skimo', '', 5.50, 0, 'picole_skimo.jpg', 1),
+	(20, 3, '', 'Picolé gianduia', '', 5.50, 0, 'img_ntf.png', 1),
+	(21, 3, '', 'Picolé Maxxi leite trufado', '', 9.00, 0, 'maxxi_black.png', 1),
+	(22, 3, '', 'Picolé Maxxi Black', '', 9.00, 0, 'maxxi_black.png', 1),
+	(23, 3, '', 'Picolé Maxxi White', '', 9.00, 0, 'maxxi_white.png', 1),
+	(24, 3, '', 'Copo Big bombom', '', 7.00, 0, 'big_bombom.png', 1),
+	(25, 3, '', 'Copo Big flocos', '', 7.00, 0, 'big_flocos.png', 1),
+	(26, 3, '', 'Copo Big napolitano', '', 7.00, 0, 'big_napolitano.png', 1),
+	(27, 3, '', 'Copo Big speciale', '', 7.00, 0, 'img_ntf.png', 1),
+	(28, 3, '', 'Mini bombom Maxxi açaí', '', 15.50, 0, 'bomb_acai.jpg', 1),
+	(29, 3, '', 'Mini bombom Maxxi skimo', '', 15.50, 0, 'bomb_skimo.png', 1),
+	(30, 3, '', 'Pote de açaí 240ml', '', 12.50, 0, 'acai_240.png', 1),
+	(31, 3, '', 'Sorvete misto', '', 2.00, 0, 'picole_misto.png', 1),
+	(32, 4, '', 'Esfirra de carne', '', 7.29, 0, 'esfirra.png', 1),
+	(33, 4, '', 'Esfirra de frango catu', '', 7.29, 0, 'esfirra.png', 1),
+	(34, 4, '', 'Enrolado de queijo', '', 7.29, 0, 'enroladinho.jpg', 1),
+	(35, 4, '', 'Enrolado de salsicha', '', 7.29, 0, 'enr_salsi.png', 1),
+	(36, 4, '', 'Hambúrguer com cheddar', '', 7.29, 0, 'bauru.png', 1),
+	(37, 4, '', 'Assado calabresa com queijo', '', 7.29, 0, 'torta.png', 1),
+	(38, 4, '', 'Torta frango catu tomate', '', 8.14, 0, 'torta.png', 1),
+	(39, 4, '', 'Torta presunto queijo catu', '', 8.14, 0, 'torta.png', 1),
+	(40, 4, '', 'Coxinha de carne', '', 7.58, 0, 'coxinha.jpg', 1),
+	(41, 4, '4', 'Coxinha de frango', '', 7.58, 0, 'coxinha.jpg', 1),
+	(42, 4, '', 'Coxinha de costela', '', 8.50, 0, 'coxinha.jpg', 1),
+	(43, 5, '', 'Pão com ovo', '', 5.62, 0, 'pao_ovo.jpg', 1),
+	(44, 5, '', 'Bauru', '', 10.66, 0, 'bauru.png', 1),
+	(45, 5, '', 'Americano', '', 18.42, 0, 'img_ntf.png', 1),
+	(46, 5, '', 'Omelete simples', '', 5.00, 0, 'omelete_sim.png', 1),
+	(47, 5, '', 'Omelete presunto e queijo', '', 7.50, 0, 'omelete_sim.png', 1),
+	(48, 5, '', 'Ovo mexido', '', 4.50, 0, 'ovo_mexido.png', 1),
+	(49, 5, '', 'Misto quente', '', 6.50, 0, 'misto_quente.png', 1),
+	(50, 6, '', 'Trento avelã', '', 4.11, 0, 'trento_avela.jpg', 1),
+	(51, 6, '', 'Trento chocolate', '', 4.11, 0, 'trento_choc.jpg', 1),
+	(52, 6, '', 'Stikadinho', '', 2.00, 0, 'stikadinho.jpg', 1),
+	(53, 6, '', 'Halls morango', '', 2.50, 0, 'halls_mor.png', 1),
+	(54, 6, '', 'Paçoca', '', 3.00, 0, 'pacoca.jpg', 1),
+	(55, 7, '', 'Trufa de brigadeiro', '', 6.00, 0, 'trufa.png', 1),
+	(56, 7, '', 'Trufa de beijinho', '', 6.00, 0, 'trufa.png', 1),
+	(57, 7, '', 'Trufa de ninho', '', 6.00, 0, 'trufa.png', 1),
+	(58, 7, '', 'Trufa Ovomaltine', '', 6.50, 0, 'trufa.png', 1),
+	(59, 7, '', 'Trufa Nutella', '', 6.50, 0, 'trufa.png', 1),
+	(60, 7, '', 'Trufa Maracujá', '', 6.50, 0, 'trufa.png', 1),
+	(61, 7, '', 'Trufa Oreo', '', 6.50, 0, 'trufa.png', 1),
+	(62, 7, '', 'Bala baiana', '', 6.00, 0, 'bala_baiana.png', 1),
+	(63, 8, '', 'Mini Coca-Cola', '', 3.00, 0, 'coca_200.png', 1),
+	(64, 8, '', 'Mini Fanta', '', 3.00, 0, 'fanta_200.png', 1),
+	(65, 8, '', 'Água', '', 2.69, 0, 'agua.jpg', 1),
+	(66, 8, '', 'Água com gás', '', 2.70, 0, 'agua_gas.jpg', 1),
+	(67, 8, '', 'Coca-Cola 2L', '', 11.97, 0, 'coca_2l.png', 1),
+	(68, 8, '', 'Fanta 2L', '', 11.50, 0, 'fanta_2l.png', 1);
 
 -- Copiando estrutura para tabela cantina.users
 DROP TABLE IF EXISTS `users`;
@@ -260,7 +266,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Copiando dados para a tabela cantina.users: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela cantina.users: ~0 rows (aproximadamente)
 DELETE FROM `users`;
 INSERT INTO `users` (`id_user`, `nome`, `cpf`, `email`, `senha`, `data_criacao`) VALUES
 	(1, 'Ana Clara', '50572398808', 'clarinhakassao@gmail.com', 'anabanana', '2026-04-16 13:04:17');
